973bbab8116f311c77287e08ae49ac7d056e7004
daniel aslan <daniel.aslan@outlook.com>
2022-06-09 20:20:27 +0000

    removed the need to get the bearer token form the environement,  It is now encyrpted in aws

--- main.py
+++ main.py
@@ -2,44 +2,54 @@
 '''
 Using this tutorial
 https://www.youtube.com/watch?v=0EekpQBEP_8
+key alias daniel-aslan-june-2022
 '''
-
 import tweepy
 import os
 from datetime import datetime
 import boto3
 
-# export 'BEARER_TOKEN'='<your_bearer_token>'
-bearer_token = os.environ.get("BEARER_TOKEN")
-
+'''
 client = tweepy.Client(bearer_token)
-#I can create a regex with vim or sed to replace this variable just once and rerun it.
+I can create a regex with vim or sed to replace this variable just once and rerun it.
+
+Global Variables:
+'''
 tag = "heatnation"
 file_name ='tweets.txt'
 file_timestamp = datetime.now().strftime('%Y%m%d')
 tweet_file = file_timestamp + file_name
+bearer = 'bearer-token-encypt'
+bucket = 'weekly-tweets-digest'
 
+# Beginning of Functions
+
+#Get the twitter bearer token as a secret from aws parameter store 
+def parameter_store():
+    ssm = boto3.client('ssm')
+    response = ssm.get_parameter(Name=bearer, WithDecryption=True)
+    bearer_token = response['Parameter']['Value']
+    return bearer_token
+
+#Search twitter from the last 7 days and create a file that will be sent to s3
 def search_tweets(tag):
+    client = tweepy.Client(parameter_store())
     query = "#{} -is:retweet".format(tag)
     with open(tweet_file, 'a+') as filehandler:
         for tweet in tweepy.Paginator(client.search_recent_tweets, query=query).flatten():
-            #print(tweet.text + '\n')
-            filehandler.write('%s\n' % tweet.text)
+            filehandler.write('%s\n------------------------------\n' % tweet.text)
 
-def send_to_s3():
+# send the file to Amazon S3
+def send_to_s3(bucket):
     s3 = boto3.client('s3')
     with open(tweet_file, "rb") as f:
-        s3.upload_fileobj(f, "weekly-tweets-digest", tweet_file)
-
-def parameter_store():
-    ssm = boto3.client('ssm')
-    response = ssm.get_parameter(Name='twitter-bearer-token', WithDecryption=False)
-    bearer_token_1 = response['Parameter']['Value']
-    return bearer_token_1
+        s3.upload_fileobj(f, bucket, tweet_file)
 
 def main():
-    search_tweets(tag)
-    send_to_s3()
+    search_tweets('HeatNation')
+    search_tweets('python3')
+    search_tweets('LabEveryday')
+    send_to_s3(bucket)
 
 if __name__ == '__main__':
     main()
